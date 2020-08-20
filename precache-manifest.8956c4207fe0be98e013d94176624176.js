ed5f74fc074328af67ba276069ed812e6ba99928
Nikolay <nikolayshutikov@gmail.com>
2020-08-20 15:41:23 +0700

    Updates

---
+++ precache-manifest.8956c4207fe0be98e013d94176624176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fa3a916bf5c6ea011033e64e0d1212",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "87182ce5cf905361e60c",
+    "url": "/react_app/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "b0488660a86afa646c5a",
+    "url": "/react_app/static/css/main.dfb3026f.chunk.css"
+  },
+  {
+    "revision": "87182ce5cf905361e60c",
+    "url": "/react_app/static/js/2.ed677b3c.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/react_app/static/js/2.ed677b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0488660a86afa646c5a",
+    "url": "/react_app/static/js/main.31ec1d4e.chunk.js"
+  },
+  {
+    "revision": "6e006a6f0dfa0d246aa9",
+    "url": "/react_app/static/js/runtime-main.65d8c773.js"
+  }
+]);
