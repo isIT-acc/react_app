6f97f848167810fa5c9e442648bf516cbd10b544
Nikolay <nikolayshutikov@gmail.com>
2020-08-20 15:19:07 +0700

    ie11 support

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -3,6 +3,7 @@ import { withStyles } from "@material-ui/core/styles";
 import ErrorIcon from "@material-ui/icons/Error";
 import AppContext from "../context/app/appContext";
 import Button from "@material-ui/core/Button";
+import { useMediaQuery } from "react-responsive";
 
 const ColorButton = withStyles((theme) => ({
   root: {
@@ -21,22 +22,35 @@ const ColorButton = withStyles((theme) => ({
 }))(Button);
 
 const Notification = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 750px)" });
   const appContext = useContext(AppContext);
   const { lastUpdateTime } = appContext;
   return (
     <div className="notification" style={{ backgroundColor: "#fde9eb" }}>
       <div className="cardContainer">
-        <div className="flexCenter">
-          <div className="flexCenter">
+        {isTabletOrMobile ? (
+          <div>
             <ErrorIcon
               style={{ color: "#ef4b53", fontSize: "2rem" }}
             ></ErrorIcon>
-            <span style={{ marginLeft: "1rem" }}>Уведомление</span>
+            <div>Уведомление</div>
+            <div>
+              {lastUpdateTime.getHours()}:{lastUpdateTime.getMinutes()}
+            </div>
           </div>
-          <div>
-            {lastUpdateTime.getHours()}:{lastUpdateTime.getMinutes()}
+        ) : (
+          <div className="flexCenter">
+            <div className="flexCenter">
+              <ErrorIcon
+                style={{ color: "#ef4b53", fontSize: "2rem" }}
+              ></ErrorIcon>
+              <span style={{ marginLeft: "1rem" }}>Уведомление</span>
+            </div>
+            <div>
+              {lastUpdateTime.getHours()}:{lastUpdateTime.getMinutes()}
+            </div>
           </div>
-        </div>
+        )}
         <div style={{ marginTop: "1rem" }}>Поступила новая рекомендация</div>
         <div style={{ marginTop: "2rem", textAlign: "right" }}>
           <ColorButton>ok</ColorButton>
